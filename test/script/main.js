571e27d3fe5db63982aedcc6fc7c1eb35076894d
pixel1004 <46800546+pixel1004@users.noreply.github.com>
2019-11-27 14:08:21 +0900

    Add files via upload

---
+++ test/script/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){//문서가 준비되면 실행
+   //setInterval(반복함수 ) 2.9초마다 ani()라는 함수 실행
+    var intv = setInterval(function(){ ani();  }, 3000);
+    function ani() {
+       //.ban의  margin:0에서 margin-left":"-1200px"으로 0.8초동안 이동해라
+       $(".ban").animate({"margin-left":"-1200px"}, 1000, 
+          function(){ /* 콜백함수*/
+           //0번째(첫이미지)는  .ban의 마지막으로 
+          // $('A').appendTo($'B')-선택자 B의 뒤에 A를 추가해라
+            $(".ban li").eq(0).appendTo($(".ban")); 
+           //원래 마진이 0으로 설정(안그러면 3번이미지가 보여지므로)
+            $(".ban").css("margin-left","0px");
+       });
+    }
+});
